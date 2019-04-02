dacacb94924949d00efcac958a9a2cef0bae3d99
Unknown <reevesbrehm@gmail.com>
2019-04-02 11:24:31 -0400

    4/2/19

---
+++ AllAboutEaston/AllAboutEaston/Views/ThingsToDo.xaml.cs
@@ -0,0 +1,28 @@
+﻿using AllAboutEaston.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AllAboutEaston.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ThingsToDo : ContentPage
+	{
+		public ThingsToDo ()
+		{
+			InitializeComponent ();
+
+            this.BindingContext = new AboutViewModel();
+        }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
